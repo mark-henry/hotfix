19cc215dfbfc49ced150423847af964955fd3700
Mark Henry <mhenry@rsimail.com>
2015-10-05 14:15:29 -0700

    Make researcher case-insensitive

--- research.py
+++ research.py
@@ -2,31 +2,31 @@ import os
 
 class Research():
     def __init__(self):
-        self.file_locations = {}
+        self._file_locations = {}
 
 
     def _fetch_locations(self, servername):
         '''Returns dict of file => list of paths, suitable for memoization of locationsfor().'''
         paths_by_file = {}
 
-        pathbase = r"\\{}\c$\rsi".format(servername)
+        pathbase = r'\\{}\c$\rsi'.format(servername)
         def substitute_base(path):
-            return "C:\\RSI" + path[len(pathbase):]
+            return 'C:\\RSI' + path[len(pathbase):]
 
         if not os.path.isdir(pathbase):
-            raise IOError("Server {} does not exist or insufficient permissions".format(servername))
+            raise IOError('Server "{}" does not exist or insufficient permissions'.format(servername))
 
         for root, directories, files in os.walk(pathbase):
             for file in files:
-                path = substitute_base(root + "\\" + file)
-                paths_by_file.setdefault(file, []).append(path)
+                path = substitute_base(root + '\\' + file)
+                paths_by_file.setdefault(file.lower(), []).append(path)
 
         return paths_by_file
 
 
     def locationsfor(self, filename, servername):
         '''Traverses C:\RSI on server and returns locations of files matching the given filename.'''
-        if servername not in self.file_locations:
-            self.file_locations[servername] = self._fetch_locations(servername)
+        if servername not in self._file_locations:
+            self._file_locations[servername] = self._fetch_locations(servername)
 
-        return self.file_locations[servername].get(filename, [])
+        return self._file_locations[servername].get(filename.lower(), [])
--- test/researchtest.py
+++ test/researchtest.py
@@ -2,28 +2,32 @@ import unittest
 from research import Research
 
 
-class ResearcherTest(unittest.TestCase):
-    def setUp(self):
-        self.r = Research()
+r = Research()
 
+
+class ResearchTest(unittest.TestCase):
     def test_server_nonexistent(self):
         with self.assertRaises(IOError):
-            self.r.locationsfor('CHWeb.dll', '0.0.0.0')
+            r.locationsfor('CHWeb.dll', '0.0.0.0')
 
     def test_file_nonexistent(self):
         self.assertEqual(
             [],
-            self.r.locationsfor('non existent file.asdf', '10.1.0.181'))
+            r.locationsfor('non existent file.asdf', '10.1.0.181'))
 
     def testCHWeb(self):
         locations = [r"C:\RSI\Web Services\NetWebServices\bin\CHWeb.dll"]
-        self.assertEqual(locations, self.r.locationsfor('CHWeb.dll', '10.1.0.181'))
+        self.assertEqual(locations, r.locationsfor('CHWeb.dll', '10.1.0.181'))
+
+    def test_caseinsensitive(self):
+        self.assertTrue(r.locationsfor('CHWeb.dll', '10.1.0.181'))
+        self.assertTrue(r.locationsfor('chweb.dll', '10.1.0.181'))
 
     def testQuickCHWeb(self):
         print("looking up first...")
-        self.r.locationsfor('CHWeb.dll', '10.1.0.181')
+        r.locationsfor('CHWeb.dll', '10.1.0.181')
         print("looking up second...")
-        self.r.locationsfor('CHWeb.dll', '10.1.0.181')
+        r.locationsfor('CHWeb.dll', '10.1.0.181')
         print("looking up third...")
-        self.r.locationsfor('CHWeb.dll', '10.1.0.181')
+        r.locationsfor('CHWeb.dll', '10.1.0.181')
         print("done!")
